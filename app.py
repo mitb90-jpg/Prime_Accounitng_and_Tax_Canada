be0507b2759df65798b26304b0930ed548d722b5
MiTahzeeb <mitb90@gmail.com>
2026-06-17 17:10:03 +0500

    Update app.py

--- app.py
+++ app.py
@@ -426,27 +426,27 @@ if uploaded_excel is not None or uploaded_pdf is not None:
             display_df[col] = display_df[col].apply(format_amount)
 
 
-# ---------------- BOLD TOTAL ROW ----------------
+    # ---------------- BOLD TOTAL ROW ----------------
 
-def bold_transaction_total(row):
+    def bold_transaction_total(row):
 
-    if row["Description"] == "TOTAL":
-        return ["font-weight: bold"] * len(row)
+        if row["Description"] == "TOTAL":
+            return ["font-weight: bold"] * len(row)
 
-    return [""] * len(row)
-
-
-styled_transactions = display_df.style.apply(
-    bold_transaction_total,
-    axis=1
-)
+        return [""] * len(row)
 
 
-st.dataframe(
-    styled_transactions,
-    use_container_width=True,
-    hide_index=True
-)
+    styled_transactions = display_df.style.apply(
+        bold_transaction_total,
+        axis=1
+    )
+
+
+    st.dataframe(
+        styled_transactions,
+        use_container_width=True,
+        hide_index=True
+    )
 
     # ---------------- CATEGORY STATUS COUNT ----------------
     total_entries = len(df)
