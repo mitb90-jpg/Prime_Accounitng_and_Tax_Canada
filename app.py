ee7850e98cf95e4d5c151f81159d27db3f2f3a18
MiTahzeeb <mitb90@gmail.com>
2026-06-20 17:01:02 +0500

    Update app.py

--- app.py
+++ app.py
@@ -282,7 +282,7 @@ def generate_invoice_pdf(invoice_number):
         ),
     ]]
 
-    contact_table = Table(contact_data, colWidths=[330, 180])
+    contact_table = Table(contact_data, colWidths=[380, 130])
     contact_table.setStyle(TableStyle([
         ("BACKGROUND", (0, 0), (-1, -1), TEAL),
         ("VALIGN", (0, 0), (-1, -1), "MIDDLE"),
