bc882f1203735d114640ee95e9620cca66e61df6
MiTahzeeb <mitb90@gmail.com>
2026-06-23 18:35:43 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2162,7 +2162,7 @@ if page == "📊 Reports":
     with visa_col1:
         with st.container(key="uploader_visa_container"):
             uploaded_visa_pdf = st.file_uploader(
-                "Upload Visa Statement PDF(s)",
+                "Upload Visa - 6023 & 7866 Statement PDF(s)",
                 type=["pdf"],
                 accept_multiple_files=True,
                 key=f"visa_pdf_uploader_{st.session_state.visa_uploader_version}"
